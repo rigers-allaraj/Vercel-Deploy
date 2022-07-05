9c0923c624d26a590d6bf7d540c8e68f56a56226
rigers <rigers.allaraj7@gmail.com>
2022-07-05 15:19:49 +0200

    09

--- index.js
+++ index.js
@@ -19,15 +19,15 @@ const resolvedFirstPath = path.resolve(fileFirstPath);
 app.use(express.static(path.join(__dirname, resolvedFirstPath)));
 
 app.get("*", function (req, res) {
-  const filePath = path.join(__dirname, "./frontend/build/index.html");
+  // const filePath = path.join(__dirname, "./frontend/build/index.html");
   // const resolvedPath = path.resolve(filePath);
-  res.sendFile(filePath,
-    function (err) {
-      if (err) {
-        res.status(500).send({"Error", err, filePath});
-      }
-    }
-  );
+  res.sendFile(filePath, path.join(__dirname, "./frontend/build/index.html"))
+    // function (err) {
+    //   if (err) {
+    //     res.status(500).send(err);
+    //   }
+    // }
+  // );
 });
 
 const port = process.env.PORT || 5000;
