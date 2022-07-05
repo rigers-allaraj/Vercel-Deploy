2a5e9d1a75df5255d3179f70cd667bf8bacf0fbd
rigers <rigers.allaraj7@gmail.com>
2022-07-05 15:42:19 +0200

    Fix 17

---
+++ frontend/src/Home.js
@@ -0,0 +1,13 @@
+import './Home.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Learn React
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import Home from './Home';
+import {
+  Route,
+  Routes,
+  Navigate,
+  BrowserRouter as Router,
+} from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Root() {
+  return (
+    <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
+    </Router>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Root />, document.getElementById('root'));
