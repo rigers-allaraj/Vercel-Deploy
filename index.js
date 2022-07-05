26333feb10bd607e11d0f1c3582d4a8160f1bfe9
rigers <rigers.allaraj7@gmail.com>
2022-07-05 12:09:48 +0200

    Fix apth again

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ app.get("/api/test", (req, res) => {
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 app.get("*", function (_, res) {
-  const filePath = "./frontend/public/index.html"
+  const filePath = "./frontend/build/index.html"
   res.sendFile(
     path.join(__dirname, filePath),
+    console.log( path.join(__dirname, filePath));
     function (err) {
       if (err) {
         res.status(500).send(err);
