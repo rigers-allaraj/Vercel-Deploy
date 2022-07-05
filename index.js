e94e10521a19716892e30a0007733c97390cc793
rigers <rigers.allaraj7@gmail.com>
2022-07-05 15:58:28 +0200

    Add proxy

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("*", function (req, res) {
   );
 });
 // "proxy": "http://localhost:3001"
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server Running on port ${port}`));
 
 module.exports = app;
