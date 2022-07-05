b45270f54185811b8c7ab69fd03b039bc12df772
rigers <rigers.allaraj7@gmail.com>
2022-07-05 15:09:28 +0200

    Fix 06

--- index.js
+++ index.js
@@ -19,10 +19,9 @@ const resolvedFirstPath = path.resolve(fileFirstPath);
 app.use(express.static(path.join(__dirname, resolvedFirstPath)));
 
 app.get("*", function (req, res) {
-  // const filePath = "./frontend/build/index.html";
+  const filePath = path.join(__dirname, "./frontend/build/index.html");
   // const resolvedPath = path.resolve(filePath);
-  res.sendFile(
-    path.join(__dirname, 'build', 'index.html'),
+  res.sendFile(filePath,
     function (err) {
       if (err) {
         res.status(500).send(err);
