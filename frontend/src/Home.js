fd78db282e81e11d1f9b2f46c8ef58140b3fe8d8
rigers <rigers.allaraj7@gmail.com>
2022-07-06 10:46:34 +0200

    SAasfa

---
+++ frontend/src/Home.js
@@ -0,0 +1,13 @@
+import './Home.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Learning ...
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './index.css';
+import ReactDOM from 'react-dom';
+import Home from './Home';
+import {
+  Route,
+  Routes,
+  Navigate,
+  BrowserRouter as Router,
+} from 'react-router-dom';
+
+function Root() {
+  return (
+    <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {/* <Route path="*" element={<Navigate replace to="/" />} /> */}
+        </Routes>
+    </Router>
+  );
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
